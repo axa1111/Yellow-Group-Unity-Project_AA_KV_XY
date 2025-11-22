2a9bf7733425def802dbde72e3c0cfc744ea5274
axa1111 <aqsa_2001@outlook.com>
2025-11-22 19:34:57 +0000

    finishing wallkie talkie, walking and fade out
    I added the walkie animation so it appears more nicely
    
    I added a next button which triggers a coroutine in the walkie talkie script to make the walkie talkie disappear using an animation and then setting it inactive,I also triggered the walking and fade out scene here

--- Assets/AxaAssets/AXAScripts/WalkieTalkieManager.cs
+++ Assets/AxaAssets/AXAScripts/WalkieTalkieManager.cs
@@ -1,17 +1,38 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 //this script will manage the walkiw talkie including the animation and audio etc. 
 public class WalkieTalkieManager : MonoBehaviour
 {
-    void Start()
+    public GameObject walkieTalkieObj;
+    private Animator walkieTalkieAnim;
+
+    public GameObject mainCameraObj;
+    private Animator mainCameraAnim;
+
+    public GameObject fadePanelObj;
+    private Animator fadePanelAnim;
+
+
+    public void HideWalkieTalkie()
     {
+        StartCoroutine(WalkiTalkieDisappears());
+        walkieTalkieAnim = walkieTalkieObj.GetComponent<Animator>();
+        mainCameraAnim = mainCameraObj.GetComponent<Animator>();
+        fadePanelAnim = fadePanelObj.GetComponent<Animator>();
         
     }
 
-
-    void Update()
+    private IEnumerator WalkiTalkieDisappears()
     {
-
+        yield return new WaitForSeconds(0.5f);
+        walkieTalkieAnim.SetBool("putWalkieTalkieAway", true);
+        yield return new WaitForSeconds(2f);
+        walkieTalkieObj.SetActive(false);
+        mainCameraAnim.SetBool("isWalking", true);
+        yield return new WaitForSeconds(8f);
+        fadePanelObj.SetActive(true);
+        //trigger next scene
     }
 }
