297be66167f905ed03196d9300d3c81db8f9353c
axa1111 <aqsa_2001@outlook.com>
2025-11-30 13:23:01 +0000

    Player movement
    - I added a script called player movement on the camera to manage the players movement, we use wasd or arrow keys to move player through scene and then we use the mouse to rotate the player/ camera to look at the environment
    
    - now i need to create a boundary so player can't leave tent (using colliders)
    
    -adjust the interaction with the saw/cloth etc.

---
+++ Assets/AxaAssets/AXAScripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+//script is on the main camera
+
+//https://medium.com/@icodewithben/3-1-creating-a-mouse-look-script-for-your-3d-character-b5c1de2a0df5 mouse rotation
+//https://docs.unity3d.com/6000.0/Documentation/Manual/class-InputManager.html = float horizontal = Input.GetAxis("Horizontal");
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Look variables")]
+    [SerializeField]
+    public float sensitivity = 300f; ///manages the sensitivity of the mouse serialize field for tesing in game scene
+    [SerializeField]
+    public Transform player; //reference to players transform which is moved (changed) using this script
+    private float xRotation = 0f; //rotation float
+
+    //movement variables
+    private float walkSpeed = 5f; //walkspeed how fast player should move
+    private CharacterController playerController; //reference to character controller component on player
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; //keep mouse centre of th scene and hide it as it's not needed 
+        playerController = player.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Rotation Using Mouse
+
+        float x = sensitivity * Input.GetAxis("Mouse X") * Time.deltaTime; //sensitiviy is muliplied by Input.GetAxis of mouse.X and then multiplied by Time.deltatime for smoother movements
+        float y = sensitivity * Input.GetAxis("Mouse Y") * Time.deltaTime; //same as the above line but for y axis of mouse 
+
+        //rotate body left and right
+        player.Rotate(Vector3.up * x); //player is being rotated on the y axis (Vector3.up = Vector(0,1,0) using the x value of the mouse )
+
+        //rotate camera up and down
+        xRotation -= y; //invert the y as mouse down is -1 and up is +1 same as XRotation += -y
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); //clamp (limit) rotation 
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); //rotate the player locaally using the xrotation we rotate around y axis
+
+        //Movement
+
+        float horizontal = Input.GetAxis("Horizontal"); //see input manager - axis in project settings this refers to D & A keys returns -1 if a (left) and +1 if right
+        float vertical = Input.GetAxis("Vertical"); //see input manager - axis in project settings this refers to w/s keys
+
+        Vector3 move = player.transform.right * horizontal + player.transform.forward * vertical; //multiply players z axiz by horizontal input value plus multiply z axis by vertical input value 
+
+        playerController.Move(move * walkSpeed * Time.deltaTime); //moving the player in the game based on input (move = wasd) multiplied by the moveSpeed multiplied by Time.deltaTime
+    }
+}
