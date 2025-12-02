50397f730fed51ef48e14c1fd7c8e5b4e042df16
axa1111 <aqsa_2001@outlook.com>
2025-12-02 21:26:22 +0000

    Fixed the camera to right foot
    - used code to move the camera to right foot when button is pressed instead of animation so if the player has rotated around the left foot it will look smoother if htey rotate
    
    - these changs were made in the Manage Camera Script
    
    - i still need to add the comments to the script

--- Assets/KvAssets/Scripts/AqsaScripts/Script_ManageCamera.cs
+++ Assets/KvAssets/Scripts/AqsaScripts/Script_ManageCamera.cs
@@ -1,12 +1,21 @@
 using UnityEngine;
 using System.Collections;
+using Unity.VisualScripting;
 
 //rotate around https://docs.unity3d.com/6000.0/Documentation/ScriptReference/Transform.RotateAround.html
+//slerp https://docs.unity3d.com/6000.0/Documentation/ScriptReference/Vector3.Slerp.html
+//set position and rotation https://docs.unity3d.com/6000.2/Documentation/ScriptReference/Transform.SetPositionAndRotation.html
 public class Script_ManageCamera : MonoBehaviour
 {
     public GameObject leftFootTarget;
     public GameObject rightFootTarget;
+
+    public Transform rightFootCameraPos;
     private GameObject currentTargetToLookAt;
+
+    private float speed = 3.3f;
+
+    private bool isMoving;
 
     private Animator cameraAnim;
 
@@ -19,39 +28,48 @@ public class Script_ManageCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.RightArrow) && currentTargetToLookAt != null)
+        if (Input.GetKey(KeyCode.RightArrow) && currentTargetToLookAt != null)
         {
             transform.RotateAround(currentTargetToLookAt.transform.position, Vector3.up, -50 * Time.deltaTime);
         }
 
-        else if(Input.GetKey(KeyCode.LeftArrow) && currentTargetToLookAt != null)
+        else if (Input.GetKey(KeyCode.LeftArrow) && currentTargetToLookAt != null)
         {
             transform.RotateAround(currentTargetToLookAt.transform.position, Vector3.up, 50 * Time.deltaTime);
         }
 
-        if(currentTargetToLookAt != null)
+        if (currentTargetToLookAt != null && !isMoving)
         {
             transform.LookAt(currentTargetToLookAt.transform);
         }
+
+        if (isMoving)
+        {
+            transform.SetPositionAndRotation(Vector3.Lerp(transform.position, rightFootCameraPos.transform.position, speed * Time.deltaTime), Quaternion.Slerp(transform.rotation, rightFootCameraPos.rotation, speed * Time.deltaTime));
+
+            if (Vector3.Distance(transform.position, rightFootCameraPos.position) < 0.1f && Quaternion.Angle(transform.rotation, rightFootCameraPos.rotation) <0.1f)
+            {
+                transform.position = rightFootCameraPos.transform.position;
+                transform.rotation = rightFootCameraPos.rotation;
+                currentTargetToLookAt = rightFootTarget;
+            }
+        }
     }
 
-//lets make the right foot button only appear when the player is in the right position for teh animation?
+    //lets make the right foot button only appear when the player is in the right position for teh animation?
     public void OnRightFootButtonClick()
     {
-        //set right foot as target and add button to trigger animation
+        isMoving = true;
 
-        cameraAnim.enabled = true;
-        StartCoroutine(WaitForRightFootAnimation());
-        //make 'inspect right foot' button inactive until 'inspect left foot' clicked?
     }
 
     public void OnLeftFootButtonClick()
     {
         //set left foot as target and add button to trigger animation
-        
+
         cameraAnim.SetBool("isLeftCloseLookButtonClicked", true);
         StartCoroutine(WaitForLeftFootAnimation());
-        
+
     }
 
     private IEnumerator WaitForLeftFootAnimation()
@@ -60,14 +78,6 @@ public class Script_ManageCamera : MonoBehaviour
         cameraAnim.enabled = false;
         currentTargetToLookAt = leftFootTarget;
     }
-
-    private IEnumerator WaitForRightFootAnimation()
-    {
-        yield return new WaitForSeconds(0.5f);
-        cameraAnim.SetBool("isRightCloseLookButtonClicked", true);
-        currentTargetToLookAt = rightFootTarget;
-        yield return new WaitForSeconds(1f);
-        cameraAnim.enabled = false;
-    }
-
 }
+
+
