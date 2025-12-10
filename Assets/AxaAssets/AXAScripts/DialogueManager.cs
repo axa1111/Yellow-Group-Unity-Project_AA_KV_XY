3ef68a8f681d0ed07f8a9f7152e8e93246abf7b8
axa1111 <aqsa_2001@outlook.com>
2025-12-10 19:36:18 +0000

    Updated comments to player pref

--- Assets/AxaAssets/AXAScripts/DialogueManager.cs
+++ Assets/AxaAssets/AXAScripts/DialogueManager.cs
@@ -2,21 +2,28 @@ using UnityEngine;
 using TMPro;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 public class DialogueManager : MonoBehaviour
 {
+    //reference to canvas obj which holds player pref script
     [SerializeField] private GameObject canvasObj;
-
+    
+    //ref to storing player name script (player prefs)
     private StoringPlayerName storingPlayerNameScript;
 
+//ref to texts which will use the name from player pref
     [SerializeField] private TextMeshProUGUI welcomeText;
     [SerializeField] private TextMeshProUGUI welcomeBackText;
     [SerializeField] private TextMeshProUGUI walkieTalkieText;
+//ref to walki talkie obj
     [SerializeField] private GameObject walkieTalkieDialogue;
 
     void Start()
     {
+        //setting storingPlayerNameScript
         storingPlayerNameScript = canvasObj.GetComponent<StoringPlayerName>();
 
+        //turning off the welcome dialogues
         welcomeText.gameObject.SetActive(false);
         welcomeBackText.gameObject.SetActive(false);
 
@@ -25,41 +32,55 @@ public class DialogueManager : MonoBehaviour
 
     void Update()
     {
+        //if we are in the main menu scene
         if (SceneManager.GetActiveScene().name == "MainMenu")
         {
+            //if the name has been saved
             if (storingPlayerNameScript.hasNameBeenSaved)
             {
+                //set welcome text active
                 welcomeText.gameObject.SetActive(true);
+                //get the name that the player input and has been saved
                 storingPlayerNameScript.playerName = PlayerPrefs.GetString("playerName");
+                //set the welcome text to read welcome doctor and add int the players name
                 welcomeText.text = "Welcome Dr " + storingPlayerNameScript.playerName;
             }
+            //otherwise if the game has been played before
             else if (storingPlayerNameScript.hasGameBeenPlayedBefore)
             {
+                //get the name
                 storingPlayerNameScript.playerName = PlayerPrefs.GetString("playerName");
+                //set the welcome back text true
                 welcomeBackText.gameObject.SetActive(true);
+                //set the text to read Welcom back Dr plus the players name
                 welcomeBackText.text = "Welcome back Dr " + storingPlayerNameScript.playerName;
             }
 
             //managing the walkie talkie text
+            //get the player name
             storingPlayerNameScript.playerName = PlayerPrefs.GetString("playerName");
-            if (!string.IsNullOrEmpty(storingPlayerNameScript.playerName))
-            {
-                walkieTalkieText.text = "Dr " + storingPlayerNameScript.playerName + " you are needed in the medic tent now... soldier with severe trench foot, amputation may be needed... over";
-            }
-        
+            //set the walkie talkie text to the outlined below and add the players name
+            walkieTalkieText.text = "Dr " + storingPlayerNameScript.playerName + " you are needed in the medic tent now... soldier with severe trench foot, amputation may be needed... over";
+
+
         }
 
     }
 
     //wait to show walkie talkie Dialogue
-
+    /*this is called on the continue buttons or the welcome dialogues
+    the wait helps it match up to the animation of the walkie
+    talkie appearing on the screen*/
     public void ShowWalkieTalkieDialogue()
     {
-        StartCoroutine(delayWalkieTalkieDialogue());
+        //StartCoroutine DelayWalkieTalkieDialogue
+        StartCoroutine(DelayWalkieTalkieDialogue());
     }
-    private IEnumerator delayWalkieTalkieDialogue()
+    private IEnumerator DelayWalkieTalkieDialogue()
     {
+        //wait for one second
         yield return new WaitForSeconds(1f);
+        //turn the walkie talkie dialogue on
         walkieTalkieDialogue.SetActive(true);
 
     }
--- Assets/AxaAssets/AXAScripts/StoringPlayerName.cs
+++ Assets/AxaAssets/AXAScripts/StoringPlayerName.cs
@@ -2,16 +2,22 @@ using UnityEngine;
 using TMPro;
 
 /*This script is working to store the players name to use in
-dialogue throughout the game*/
+dialogue throughout the game this script sits on the camera Obj in Main menu scene*/
 
 public class StoringPlayerName : MonoBehaviour
 {
+    //text player inputs in
     [SerializeField] private TextMeshProUGUI myText;
 
+    //input field parent obj
     [SerializeField] private GameObject inputNameParentObj;
 
+    //players name variable
     public string playerName;
+    //bool to check if name is saved
     public bool hasNameBeenSaved;
+
+    //bool to check if game has been played before
     public bool hasGameBeenPlayedBefore = false;
     
     void Start()
@@ -20,39 +26,52 @@ public class StoringPlayerName : MonoBehaviour
         hasNameBeenSaved = false;
     }
 
+    /*When player presses play button this is called (in its on click event)
+    checkes to see if we need a name from the player or if we 
+    already stored and setting input field active or inactive based on that*/
     public void OnPlayClick()
     {
+        //if the name has already been stored before
         if (PlayerPrefs.HasKey("playerName"))
         {
+            //turn off the input name obj
             inputNameParentObj.SetActive(false);
+            //set bool to true
             hasGameBeenPlayedBefore = true;
         }
         else
         {
+            //otherwise turn on input field and set bool to false
             hasGameBeenPlayedBefore = false;
             inputNameParentObj.SetActive(true);
         }
     }
+
+    /*method called on the onClick event of the save button next to the input
+    field, here we are saving the name*/
     public void OnClickSaveName()
     {
-            playerName = myText.text;
-            PlayerPrefs.SetString("playerName", myText.text);
-            PlayerPrefs.Save();
-            inputNameParentObj.SetActive(false);
-            hasNameBeenSaved = true;    
+
+        /* saveing the text the player put in storing it under playerName key */
+        PlayerPrefs.SetString("playerName", myText.text);
+        //saving onto disk
+        PlayerPrefs.Save();
+        //disable the input field obj
+        inputNameParentObj.SetActive(false);
+        //bool set to true
+        hasNameBeenSaved = true;
     }
 
-    public void OnClickGet()
-    {
-        playerName = PlayerPrefs.GetString("playerName");
-        Debug.Log("Welcome back to the app " + playerName);
-    }
-
+    //this is on the OnClickEvent on the change name button
+    //we delete the key and turn the input field on again
     public void OnClickChangeName()
     {
+        //setting bools to false
         hasGameBeenPlayedBefore = false;
         hasNameBeenSaved = false;
+        //deleting the key
         PlayerPrefs.DeleteKey("playerName");
+        //setting input field active 
         inputNameParentObj.SetActive(true);
     }
 }
