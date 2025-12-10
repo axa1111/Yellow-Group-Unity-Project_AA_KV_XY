aa83f3b64a38e517a8ba801221623956518d56ba
axa1111 <aqsa_2001@outlook.com>
2025-12-10 19:48:44 +0000

    Added Comments to Walkie Talkie manager

--- Assets/AxaAssets/AXAScripts/WalkieTalkieManager.cs
+++ Assets/AxaAssets/AXAScripts/WalkieTalkieManager.cs
@@ -1,16 +1,24 @@
 using System.Collections;
+using System.Configuration;
 using UnityEngine;
 
 //this script will manage the walkiw talkie including the animation and audio etc. 
 public class WalkieTalkieManager : MonoBehaviour
 {
+    //ref to walkie talkie obj
     public GameObject walkieTalkieObj;
+    //ref to walkie talkie animator
     private Animator walkieTalkieAnim;
 
+    //REF TO MAIN CAMERA OBJ
     public GameObject mainCameraObj;
+    //ref to main cam animation
     private Animator mainCameraAnim;
 
+    //ref to fade panel
     private GameObject fadePanelObj;
+
+   //ref to canvas 
     private GameObject canvasObj;
 
     //audio source
@@ -18,25 +26,40 @@ public class WalkieTalkieManager : MonoBehaviour
 
     public void Start()
     {
+        //setting walkie talkie animator
         walkieTalkieAnim = walkieTalkieObj.GetComponent<Animator>();
+        //setting camera animator
         mainCameraAnim = mainCameraObj.GetComponent<Animator>();
+        //setting vanvas through tag
         canvasObj = GameObject.FindGameObjectWithTag("GameManagerCanvas");
+        //setting fade panel
         fadePanelObj = canvasObj.transform.Find("Fade_ToDark_Panel").gameObject;
+        //setting audiosource on camera
         cameraAS = mainCameraObj.GetComponent<AudioSource>();
     }
-    public void HideWalkieTalkie()
+    
+    //Method called on next button 
+        public void HideWalkieTalkie()
     {
+        //Start Coroutine WalkieTalkie Disapears
         StartCoroutine(WalkiTalkieDisappears());
     }
 
     private IEnumerator WalkiTalkieDisappears()
     {
+        //wait for half a second
         yield return new WaitForSeconds(0.5f);
+        //set bool true to trigger animation
         walkieTalkieAnim.SetBool("putWalkieTalkieAway", true);
+        //waot 2 seconds
         yield return new WaitForSeconds(2f);
+        //set bool to true to trigger walking animation
         mainCameraAnim.SetBool("isWalking", true);
+        //play the footsteps audio
         cameraAS.Play();
+        //wait 8 seconds
         yield return new WaitForSeconds(8f);
+        //turn on the fade panel
         fadePanelObj.SetActive(true);
 
     }
